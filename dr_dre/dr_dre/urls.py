86a38eaaef40a94bf734674786f388e48f92b641
abbddos <abbddos@gmail.com>
2021-08-21 23:42:51 +0300

    all done... happy new app

--- dr_dre/dr_dre/urls.py
+++ dr_dre/dr_dre/urls.py
@@ -32,5 +32,9 @@ urlpatterns = [
     path('profile/<uid>', dre_views.profile, name = 'profile'),
     path('change_password/<uid>', dre_views.change_password, name = 'change_password'),
     path('login/', auth_views.LoginView.as_view(template_name = 'login/login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'login/logout.html'), name = 'logout'),    
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'login/logout.html'), name = 'logout'), 
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = 'login/reset_password.html'), name = 'reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = 'login/password_reset_sent.html'), name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = 'login/password_reset_form.html'), name = 'password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = 'login/password_reset_done.html'), name = 'password_reset_complete')
 ]
