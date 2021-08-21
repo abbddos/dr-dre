bfce8dfa8b1706537aa33d2d98f743f0936d7b14
abbddos <abbddos@gmail.com>
2021-08-22 00:12:02 +0300

    some modifications

--- dr_dre/dr_dre/settings.py
+++ dr_dre/dr_dre/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'dr_dre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dr_dre_db',
+        'USER': 'abdul',
+        'PASSWORD': 'abdul*1983',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
